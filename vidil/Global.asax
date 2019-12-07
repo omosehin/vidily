e672ec1a1c6e5f23691ed211b1d58838a5d2a5e5
Abayomi <omosehin14@gmail.com>
2019-12-07 13:18:00 +0100

    Add project files.

---
+++ vidil/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vidil.MvcApplication" Language="C#" %>
---
+++ vidil/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vidil.Startup))]
+namespace vidil
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
